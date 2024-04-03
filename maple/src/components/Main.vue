6731b439c08cc9a2f7a83013e4a9b15892ab9597
Noh-jisu <nojisu123@naver.com>
2024-04-03 21:00:59 +0900

    axios BaseUrl 설정 및 Router 설정

--- maple/src/components/Main.vue
+++ maple/src/components/Main.vue
@@ -1,41 +1,36 @@
 <template>
   <main>
-    <div>
-      <input type="text" v-model="nickName" @keyup.enter="searchInfo">
+    <div class="test">
+      <input class="input-test" type="text" v-model="nickName" @keyup.enter="searchInfo">
       <button @click="searchInfo">search</button>
     </div>
   </main>
 </template>
 
 <script>
-import axios from 'axios';
 
 export default {
   data: () => ({
+    apiKey: 'test_99c9ccd90bcabd0adda2a298ee3a34b554b161560510a7c43d80ffded1737b6ec992ce9ee48acd724627223fa2b6d1c5',
     nickName: '',
   }),
   methods: {
     searchInfo: function () {
-      // const headers = {
-      //   'x-nxopen-api-key': 'test_99c9ccd90bcabd0adda2a298ee3a34b554b161560510a7c43d80ffded1737b6ec992ce9ee48acd724627223fa2b6d1c5',
-      // }
-      const baseURL = axios.create({
-        baseURL: 'https://open.api.nexon.com'
-      });
-      baseURL.get('/maplestory/v1/id', {
+      this.axios.get('/maplestory/v1/id', {
           params: { character_name: this.nickName },
           headers: {
-            'x-nxopen-api-key': 'test_99c9ccd90bcabd0adda2a298ee3a34b554b161560510a7c43d80ffded1737b6ec992ce9ee48acd724627223fa2b6d1c5',
+            'x-nxopen-api-key': this.apiKey,
           },
         }).then((result) => {
           if (result.status === 200) {
-            baseURL.get('/maplestory/v1/character/basic', {
+            // axios 기본설정 방법 찾기
+            this.axios.get('/maplestory/v1/character/basic', {
               params: {
                 ocid: result.data.ocid,
                 date: '2024-04-01',
               },
               headers: {
-                'x-nxopen-api-key': 'test_99c9ccd90bcabd0adda2a298ee3a34b554b161560510a7c43d80ffded1737b6ec992ce9ee48acd724627223fa2b6d1c5',
+                'x-nxopen-api-key': this.apiKey,
               },
             })
             .then((result) => {
--- maple/src/main.js
+++ maple/src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import router from './resources/router/index.js'
+
+import './assets/scss/ui.scss';
 
 const app = createApp(App);
 
+axios.defaults.baseURL = 'https://open.api.nexon.com'
 app.config.globalProperties.axios = axios;
-app.mount("#app");
+app.use(router).mount("#app");
 
